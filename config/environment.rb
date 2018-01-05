fecc98e8a0fe68d5ffe1db308fe96bd3173f5091
Meredith Reece <mardebugger@gmail.com>
2018-01-05 05:05:20 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -18,3 +18,17 @@ def drop_db
     DB.execute("DROP TABLE #{table}")
   end
 end
+
+connection = ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/students.sqlite"
+  )
+
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY,
+    name TEXT
+  )
+  SQL
+
+ActiveRecord::Base.connection.execute(sql)
